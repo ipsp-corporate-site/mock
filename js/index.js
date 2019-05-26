735497ee6e569d376cc6dbb5c6df41cb1e0e6889
togo_eitetsu <togo@pikon.jp>
2019-05-26 13:09:08 +0900

    modify h1 font-size,video width

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 $(function(){
 	$(".btn-gnavi").on("click", function(){
+		// ハンバーガーメニューの位置を設定するための変数
+		var rightVal = 0;
 		if($(this).hasClass("btn-open")) {
 			$(this).removeClass("btn-open");
+          $("nav").removeClass("nav-open");
 		} else {
 			$(this).addClass("btn-open");
+          $("nav").addClass("nav-open");
 		}
-      if($(".global-navi").hasClass("nav-open")){
-          $(this).removeClass("nav-open");
-        } else {
-          $(this).addClass("nav-open");
-        }
 	});
 });
